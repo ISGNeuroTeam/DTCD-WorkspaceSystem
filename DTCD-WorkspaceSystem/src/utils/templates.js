c4c0d5f0588d230ade35e068db284266d380d768
rkuramshin <rkuramshin@isgneuro.com>
2022-05-18 15:21:06 +0400

    release v0.5.0

--- DTCD-WorkspaceSystem/src/utils/templates.js
+++ DTCD-WorkspaceSystem/src/utils/templates.js
@@ -3,9 +3,4 @@ export function toMountTemplates() {
   gridBody.setAttribute('class', 'grid-stack');
   gridBody.style = 'width:100%;height:100%';
   document.body.appendChild(gridBody);
-
-  const footer = document.createElement('div');
-  footer.innerHTML = footerTemplate;
-  footer.classList.add('workspace-footer');
-  document.body.appendChild(footer);
 }
